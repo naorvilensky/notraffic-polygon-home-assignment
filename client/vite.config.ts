0b0f5b85a7e71fd61fee55caa91fe148d7fba30f
naor.vilensky <naor.vilensky@gmail.com>
2025-10-30 21:13:35 +0200

    packages + configs for prettier imports

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			'@src': path.resolve(__dirname, './src'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@interfaces': path.resolve(__dirname, './src/interfaces'),
+			'@utils': path.resolve(__dirname, './src/utils'),
+		},
+	},
 });
