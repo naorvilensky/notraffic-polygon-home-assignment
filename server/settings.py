e7bae4d05fe28d4ff05b3327959869f1f22e6dbf
naor.vilensky <naor.vilensky@gmail.com>
2025-10-31 13:48:40 +0200

    made the server work with docker-compose

--- server/settings.py
+++ server/settings.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     CORS_ORIGINS: List[str] = ["http://localhost:5173"]
 
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file="../.env",
         env_file_encoding="utf-8",
         extra="ignore",
         case_sensitive=True,
